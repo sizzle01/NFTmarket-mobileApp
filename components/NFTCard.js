e541840847d607dcccb25af0cd79def777f59586
Your Name <you@example.com>
2022-05-25 17:11:12 +0100

    styling NFT cards

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,9 +1,18 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Image, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-const NFTCard = () => {
+import { COLORS, SIZES, SHADOWS, assets } from '../constants';
+
+const NFTCard = ({data}) => {
+    const navigation = useNavigation()
   return (
-    <View>
+    <View style={{
+        backgroundColor: COLORS.white,
+        borderRadius: SIZES.font,
+        marginBottom: SIZES.extraLarge,
+        margin: SIZES.base,
+        ...SHADOWS.dark
+    }}>
       <Text>NFTCard</Text>
     </View>
   )
